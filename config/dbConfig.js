0deba293c2e98a5e6b495b686365ca5cc9a1b261
Nalipp <nateplipp@gmail.com>
2017-08-23 14:59:30 +0900

    set up database connections

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,16 +1,23 @@
 var mongoose = require('mongoose');
+var url;
+
+mongoose.Promise = global.Promise;
+
+/* eslint-disable no-console */
 
 if (process.env.NODE_ENV === 'dev') {
-  url = 'mongodb://localhost:27017/nodeToyDev';
+  url = 'mongodb://localhost:27017/tstoryDev';
+} else if (process.env.NODE_ENV === 'test' ) {
+  url = 'mongodb://localhost/tstoryTest'
 } else {
-  url = 'mongodb://localhost:27017/nodeToy';
+  url = 'mongodb://heroku_dq13zd86:q5eilfck8e418hr07rtk8v2tqj@ds151433.mlab.com:51433/heroku_dq13zd86';
 }
 
 mongoose.connect(url, err => {
   if (err) {
     console.log("# Failed to connect to MongoDB ");
   } else {
-    console.log('# Connected to MongoDB')
+    console.log('# Connected to MongoDB', url)
   }
 })
 
--- test/test_helper.js
+++ test/test_helper.js
@@ -2,15 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-before((done) => {
-  mongoose.connect('mongodb://localhost/nodeToyTest');
-  mongoose.connection
-    .once('open', () => done())
-    .on('error', (error) => {
-      console.warn('Mongoose Warning', error);
-    });
-});
-
 beforeEach((done) => {
   const { users } = mongoose.connection.collections;
   users.drop(() => done());
