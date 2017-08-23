4fd4231dd4723f0e9a368fabbd7d5955d8238466
Nalipp <nateplipp@gmail.com>
2017-08-23 17:37:05 +0900

    slip model validation tests

--- models/slip.js
+++ models/slip.js
@@ -8,40 +8,42 @@ const SlipSchema = new mongoose.Schema({
     validate: {
       validator: (language) => 
       language.length > 1 && language.length < 100,
-      message: 'Language must be valid length'
+      message: 'Language must be a valid length' 
     }
   },
-  sub_languages: String,
+  sub_languages: {
+    type: String,
+    validate: {
+      validator: (language) => language.length < 200,
+      message: 'Sub languages must be less than 200 characters total'
+    }
+  },
   study_type: {
     type: String,
     validate: {
-      validator: (study_type) => 
-      study_type.length > 1 && study_type.length < 100,
-      message: 'Study type must be valid length'
+      validator: (study_type) => study_type.length < 100,
+      message: 'Study type is too long'
     }
   },
   resource_type: {
     type: String,
     validate: {
-      validator: (resource_type) => 
-      resource_type.length > 1 && resource_type.length < 100,
-      message: 'Resource type must be valid length'
+      validator: (resource_type) => resource_type.length < 100,
+      message: 'Resource type is too long'
     }
   },
   resource_title: {
     type: String,
     validate: {
-      validator: (resource_title) => 
-      resource_title.length > 1 && resource_title.length < 100,
-      message: 'Resource title must be valid length'
+      validator: (resource_title) => resource_title.length < 100,
+      message: 'Resource title is too long'
     }
   },
   description: {
     type: String,
     validate: {
-      validator: (description) => 
-      description.length > 1 && description.length < 400,
-      message: 'Description must be valid length'
+      validator: (description) => description.length < 400,
+      message: 'Description is too long'
     }
   },
   url: {
---
+++ test/slip/modelValidations.tests.js
@@ -0,0 +1,92 @@
+const assert = require('assert');
+const Slip = require('../../models/slip');
+
+beforeEach((done) => {
+  Slip.remove({}).then(() => {
+    done(); 
+  });
+});
+
+describe('validates Slip model', () => {
+
+  it('Should require a Language', () => {
+    const slip = new Slip({
+      language: undefined
+    });
+    const validationResult = slip.validateSync();
+    const { message } = validationResult.errors.language;
+    assert(message === 'Language is required');
+  });
+
+  it('Should validate Language length > 1', () => {
+    const slip = new Slip({
+      language: 'a',
+    });
+    const validationResult = slip.validateSync();
+    const { message } = validationResult.errors.language;
+    assert(message === 'Language must be a valid length');
+  });
+
+  it('Should validate Language length < 100', () => {
+    const slip = new Slip({
+      language: 'a'.repeat(100),
+    });
+    const validationResult = slip.validateSync();
+    const { message } = validationResult.errors.language;
+    assert(message === 'Language must be a valid length');
+  });
+
+  it('Should validate sub_languages length < 200', () => {
+    const slip = new Slip({
+      sub_languages: 'a'.repeat(200),
+    });
+    const validationResult = slip.validateSync();
+    const { message } = validationResult.errors.sub_languages;
+    assert(message === 'Sub languages must be less than 200 characters total');
+  });
+
+  it('Should validate Study_type length < 100', () => {
+    const slip = new Slip({
+      study_type: 'a'.repeat(100),
+    });
+    const validationResult = slip.validateSync();
+    const { message } = validationResult.errors.study_type;
+    assert(message === 'Study type is too long');
+  });
+
+  it('Should validate Resource_type length < 100', () => {
+    const slip = new Slip({
+      resource_type: 'a'.repeat(100),
+    });
+    const validationResult = slip.validateSync();
+    const { message } = validationResult.errors.resource_type;
+    assert(message === 'Resource type is too long');
+  });
+
+  it('Should validate Resource_title length < 100', () => {
+    const slip = new Slip({
+      resource_title: 'a'.repeat(100),
+    });
+    const validationResult = slip.validateSync();
+    const { message } = validationResult.errors.resource_title;
+    assert(message === 'Resource title is too long');
+  });
+
+  it('Should validate Description length < 400', () => {
+    const slip = new Slip({
+      description: 'a'.repeat(400),
+    });
+    const validationResult = slip.validateSync();
+    const { message } = validationResult.errors.description;
+    assert(message === 'Description is too long');
+  });
+
+  it('Should validate Url as valid', () => {
+    const slip = new Slip({
+      url: 'www.'
+    });
+    const validationResult = slip.validateSync(); 
+    const { message } = validationResult.errors.url;
+    assert(message === 'Url must be valid');
+  });
+});
