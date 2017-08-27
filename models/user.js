5f3a531c6db9967ee0a068401f448c1f8e705664
Nalipp <nateplipp@gmail.com>
2017-08-27 21:44:06 +0900

    improve total hours virtual

--- models/user.js
+++ models/user.js
@@ -35,12 +35,13 @@ UserSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
 };
 
-UserSchema.virtual('slipCount').get(function() {
+UserSchema.virtual('totalHours').get(function() {
   let totalMinutes = 0; 
   this.slips.forEach((total) => {
     if (total.minutes) totalMinutes += total.minutes
   });
-  return Math.floor(totalMinutes / 60) + ' hours';
+  const totalHours = Math.floor(totalMinutes / 60);
+  return totalHours > 1 ? ' | ' + totalHours + ' hours' : '';
 });
 
 UserSchema.plugin(uniqueValidator, { message: '{PATH} must be unique' });
